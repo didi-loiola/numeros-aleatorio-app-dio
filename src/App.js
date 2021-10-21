1c7d24905c7cffd9afd35bc582c8798ca200aac8
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-21 17:23:02 -0300

    Função que gera os números aleatórios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {Text,SafeAreaView, TouchableOpacity,StyleSheet} from 'react-native';
 
 const App = () => {
+
+    const [numero, setNumero] = useState(0);
+
+    function handleNumero() {
+        const novo_numero = Math.floor(Math.random() * 100)
+        setNumero(novo_numero)
+    }
+
     return (
         <SafeAreaView  style={style.container}>
-            <Text style={style.numero}>0</Text>
-            <TouchableOpacity style={style.botao}>
+            <Text style={style.numero}>{numero}</Text>
+            <TouchableOpacity onPress={handleNumero} style={style.botao}>
                 <Text style={style.texto}>Gerar número</Text>
             </TouchableOpacity>
         </SafeAreaView>
